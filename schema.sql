d1668ac68a06cc8424ee972f0f27f40905cea58c
Iyra <iyra@geist.my.domain>
2019-02-10 22:21:08 +0000

    added db library and two tables for collections

--- schema.sql
+++ schema.sql
@@ -7,5 +7,21 @@ drop table if exists users;
     realname text not null,
     born integer,
     gender text,
-    parentid integer not null
+    parentid integer not null -- a parent id > 0 means that it's a child account
+);
+
+drop table if exists collections;
+create table collections (
+       id integer primary key autoincrement,
+       title text not null,
+       xmldata text not null
+);
+
+-- table to associate collections with users (i.e to show who owns which collections)
+drop table if exists collection_owners;
+create table collection_owners (
+       uid integer not null,
+       cid integer not null,
+       foreign key(uid) references users(id),
+       foreign key(cid) references collections(id)
 );
